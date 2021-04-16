e7d29737500722a8aa06dc7fb5cae8491d71f65b
José Antonio Zamudio <joseantoniozamudio11@gmail.com>
2021-04-16 10:00:20 +0200

    added calendar

--- src/main/java/org/springframework/samples/petclinic/repository/SpecialtyRepository.java
+++
@@ -1,9 +0,0 @@
-
-package org.springframework.samples.petclinic.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.samples.petclinic.model.Specialty;
-
-public interface SpecialtyRepository extends JpaRepository<Specialty, Integer> {
-
-}
