d77ec1c4da0e94d22803de2ee2250ca9deb0f5a2
JJosePP <jjperezplata@gmail.com>
2021-05-21 19:57:24 +0200

    Validation create and update veterinarians

---
+++ src/main/java/org/springframework/samples/petclinic/util/VetValidator.java
@@ -0,0 +1,30 @@
+package org.springframework.samples.petclinic.util;
+
+import org.springframework.samples.petclinic.model.FormVetType;
+
+public class VetValidator {
+
+	// el siguiente regex no permite número ni caracteres especiales, solo letras del alfabeto(incluida la ñ) con sus tildes.
+	private static final String regex = "^[a-zA-ZÀ-ÿ\\u00f1\\u00d1]+(\\s*[a-zA-ZÀ-ÿ\\u00f1\\u00d1]*)*[a-zA-ZÀ-ÿ\\u00f1\\u00d1]+$";
+	private VetValidator() {
+		new VetValidator();
+	}
+	
+	public static boolean validateNameVet(FormVetType vet) {
+		boolean res = false;
+		if(!vet.getFirstName().matches(regex)) {
+			res = true;
+		}
+		
+		return res;
+	}
+	
+	public static boolean validateLastNameVe(FormVetType vet) {
+		boolean res = false;
+		if(!vet.getLastName().matches(regex)) {
+			res = true;
+		}
+		
+		return res;
+	}
+}
